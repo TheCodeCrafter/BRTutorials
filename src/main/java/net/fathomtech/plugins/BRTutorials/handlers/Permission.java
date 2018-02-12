032b86417fb90729cf08d3f3d067979eda1c9636
Noah <noahcoetsee@gmail.com>
2018-02-12 10:15:38 -0500

    Create Permission.java

---
+++ src/main/java/net/fathomtech/plugins/BRTutorials/handlers/Permission.java
@@ -0,0 +1,8 @@
+package net.fathomtech.plugins.BRTutorials.handlers;
+
+public enum Permission {
+  Permission(String permission) {
+    this.permission = permission;
+    return permission;
+  }
+}
