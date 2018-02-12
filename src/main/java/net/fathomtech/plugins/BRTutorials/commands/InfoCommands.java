dd9259fccd70217826be5df1136db0bb644aeba7
Noah <noahcoetsee@gmail.com>
2018-02-12 10:12:59 -0500

    Create InfoCommands.java

---
+++ src/main/java/net/fathomtech/plugins/BRTutorials/commands/InfoCommands.java
@@ -0,0 +1,10 @@
+package net.fathomtech.plugins.BRTutorials.commands;
+
+public class InfoCommands implements CommandExecutor {
+
+  @Override
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+  
+  }
+  
+}
