1e5f196c03f9121ea68bf85ed6f60f6379d27ce1
Noah <noahcoetsee@gmail.com>
2018-02-12 12:49:33 -0500

    Create ChatListener.java

---
+++ src/main/java/net/fathomtech/plugins/BRTutorials/listeners/ChatListener.java
@@ -0,0 +1,8 @@
+package net.fathomtech.plugins.BRTutorials.listeners;
+
+public class ChatListener implements Listener {
+  @EventHandler
+  public void onPlayerChat(AsyncPlayerChatEvent event) {
+  
+  }
+}
