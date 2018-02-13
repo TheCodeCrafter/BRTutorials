bd910bcc191cbd59075fa4da9f5558571b61333e
Noah <noahcoetsee@gmail.com>
2018-02-13 12:54:20 -0500

    Update Tutorial.java

--- src/main/java/net/fathomtech/plugins/BRTutorials/handlers/Tutorial.java
+++ src/main/java/net/fathomtech/plugins/BRTutorials/handlers/Tutorial.java
@@ -1,5 +1,16 @@
 package net.fathomtech.plugins.BRTutorials.handlers;
 
 public class Tutorial {
+  public String name;
   
+  public ArrayList<Step> steps;
+  public int steps;
+  
+  public String prefix;
+  public String suffix;
+  
+  public String startMessage;
+  public String endMessage;
+  
+  public Permission permission;
 }
