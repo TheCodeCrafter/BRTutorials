d8d09927123119ebb10b2c531f256800575eb7a2
Noah <noahcoetsee@gmail.com>
2018-02-13 09:10:35 -0500

    Update InfoCommands.java

--- src/main/java/net/fathomtech/plugins/BRTutorials/commands/InfoCommands.java
+++ src/main/java/net/fathomtech/plugins/BRTutorials/commands/InfoCommands.java
@@ -4,7 +4,13 @@ public class InfoCommands implements CommandExecutor {
 
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-  
+    if(sender.hasPermission(Permission.ADMIN.NODE) {
+      for(String message : InfoMessage.getMessage()) {
+        sender.sendMessage(message);
+        return true;
+      }
+    } else {
+      return false;
+    }
   }
-  
 }
