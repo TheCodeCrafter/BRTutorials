b227d3904afe79724e9cad960856ca21a4f064c2
Noah <noahcoetsee@gmail.com>
2018-02-13 14:47:35 -0500

    Update Tutorial.java

--- src/main/java/net/fathomtech/plugins/BRTutorials/handlers/Tutorial.java
+++ src/main/java/net/fathomtech/plugins/BRTutorials/handlers/Tutorial.java
@@ -4,7 +4,7 @@ public class Tutorial {
   public String name;
   
   public ArrayList<Step> steps;
-  public int steps;
+  public int stepCount;
   
   public String prefix;
   public String suffix;
@@ -13,4 +13,55 @@ public class Tutorial {
   public String endMessage;
   
   public Permission permission;
+  
+  public Tutorial(String n, ArrayList<Step> s, String pre, String suf, String sMessage, String eMessage, Permission perm) {
+    name = n;
+    steps = s;
+    stepCount = s.length;
+    prefix = pre;
+    suffix = suf;
+    startMessage = sMessage;
+    endMessage = eMessage;
+    
+    if(perm == null) {
+      permission = Permission.START.NODE;
+    } else {
+      permission = perm;
+    }
+  }
+  
+  // GETTERS
+  public String getName() {
+    return this.name;
+  }
+  
+  public ArrayList<Steps> getSteps() {
+    return this.steps;
+  }
+  
+  public int getStepCount() {
+    return this.stepCount;
+  }
+  
+  public String getPrefix() {
+    return this.prefix;
+  }
+  
+  public String getSuffix() {
+    return this.suffix;
+  }
+  
+  public String getStartMessage() {
+    return this.startMessage;
+  }
+  
+  public String getEndMessage() {
+    return this.endMessage;
+  }
+  
+  // SETTERS
+  
+  public void setName(String newName) {
+    this.name = newName;
+  }
 }
