09e211f203f0c1bc755f3ad5c8cb9c6c0126c6f4
Noah <noahcoetsee@gmail.com>
2018-02-14 12:18:05 -0500

    Update BRTutorialPlugin.java

--- src/main/java/net/fathomtech/plugins/BRTutorials/BRTutorialPlugin.java
+++ src/main/java/net/fathomtech/plugins/BRTutorials/BRTutorialPlugin.java
@@ -1,13 +1,38 @@
 package net.fathomtech.plugins.BRTutorials;
 
 public class BRTutorialPlugin extends JavaPlugin {
+  
+  public HashMap<OfflinePlayer, Tutorial> tutorialQueue = new HashMap<OfflinePlayer, Tutorial>();
+  public HashMap<String, Tutorial> tutorials = new HashMap<String, Tutorial>();
+  
+  public Configuration config = new Configuration();
+  
   @Override
   public void onEnable() {
-  
+    // LOAD CONFIG
+    getLogger().info(ChatColor.YELLOW + "Loading configuration...");
+    config.loadConfig();
+    getLogger().info(ChatColor.GREEN + "Configuration loaded!");
+    
+    // LOAD TUTORIALS FROM CONFIG
+    getLogger().info(ChatColor.YELLOW + "Loading tutorials...");
+    tutorials = this.loadTutorials();
+    getLogger().info(ChatColor.GREEN + "Tutorials loaded!");
   }
   
   @Override
   public void onDisable() {
+    // PLACEHOLDER MESSAGES
+    getLogger().info("Checking config for changes...");
+    getLogger.info("Unloading data...");
+  }
   
+  public void reloadConfig() {
+    this.getConfig().reload();
+    this.loadTutorials();
+  }
+  
+  public void loadTutorials() {
+    this.tutorials = this.config.loadTutorials();
   }
 }
